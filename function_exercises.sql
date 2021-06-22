863bbe25315c4e0e42f49d79412a33927373f6e7
Jordan Felan <felan.jordan94@gmail.com>
2021-06-22 09:24:39 -0500

    exercise completed

---
+++ function_exercises.sql
@@ -0,0 +1,32 @@
+USE employees;
+
+#ORDER BY EXERCISES
+
+SELECT emp_no, first_name, last_name FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name;
+
+SELECT emp_no, first_name, last_name FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name, last_name;
+
+SELECT emp_no, first_name, last_name FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name, first_name;
+
+
+SELECT first_name, last_name FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no;
+SELECT first_name, last_name FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no DESC;
+
+SELECT first_name, last_name FROM employees WHERE last_name LIKE '%q%';
+
+#PART 2
+SELECT emp_no, first_name, last_name FROM employees
+WHERE gender = 'M'
+    AND first_name = 'Irena'
+   OR first_name = 'Vidya'
+   OR first_name = 'Maya';
+
+SELECT first_name, last_name FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+
+#updated
+SELECT CONCAT(first_name, last_name) FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
+
+SELECT first_name, last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
